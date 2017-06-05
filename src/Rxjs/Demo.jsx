49af7c52f6f766d4abb56f1c7f6068e943591e6d
saberteeth <frostmourner@outlook.com>
2017-06-05 12:30:03 +0800

    rxjs

--- src/Rxjs/Demo.jsx
+++ src/Rxjs/Demo.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
 import {Event, Active} from './Active'
 
-const event = new Event((observer)=>{
-  observer.next(1);
-  throw new Error(1);
-});
-
-export default() => {
-  const click = event.getAction((observable)=>{
-    return observable.map(e=>{
-      return {event:e.nextEvent}
-    });
-  },{
-    next: x => console.log('got value ',x),
-    error: err => console.error('something wrong occurred: 404',err),
-    complete: () => console.log('done'),
-  });
-
-  return (
-    <div>
-      Please open log to see result. U can't submit a lot time in a second.
-      <br/>
-      <button onClick={click}>submit</button>
-    </div>
-  )
+const Type = {
+  LOADING:0,
+  LOADEND:1,
+  LOADERR:2
 }
+class Demo extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      status: 3,
+      value: 0  
+    }
+    const thas = this;
+    this._event = new Event((observer)=>{
+      observer.next({status:Type.LOADING});
+      setTimeout(function() {
+        try{
+          const value = ++thas.state.value;
+          if(value > 1){
+            throw new Error('bad data!');
+          }
+          observer.next({status:Type.LOADEND,value:value});
+        }catch(err){
+          observer.next({status:Type.LOADERR});  
+        }finally{
+          observer.complete();
+        }
+      }, 2000);
+    });
+    this.initEvent();
+  }
+
+  initEvent(){
+    this.click = this._event.getAction((observable)=>{
+      return observable.map(e=>{
+        return {...e.nextEvent};
+      });
+    },{
+      next: x => {
+        this.setState(Object.assign({},this.state,x));
+      },
+      error: err => {
+        this.setState({status:Type.LOADERR});
+      },
+      complete: () => {
+        console.log('end.')
+      }
+    });
+  }
+
+  render(){
+    const str = ["Loading...",
+    "Load data is " + this.state.value,
+    "T_T! 404, Can't find anything.",
+    "Click button."];
+    return (
+      <div>
+        {str[this.state.status]}
+        <br/>
+        <button onClick={this.click}>submit</button>
+      </div>
+    )
+  }
+}
+
+export default Demo;
