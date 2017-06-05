95b52d97c03f0d56d1cd8281296817fe83f2092e
saberteeth <frostmourner@outlook.com>
2017-06-05 11:22:06 +0800

    rxjs

--- src/Rxjs/Active.jsx
+++ src/Rxjs/Active.jsx
@@ -8,11 +8,49 @@ export class Active {
   init(rule){
     this._subject = new Rx.Subject();
     rule(this._subject);
+    this.insert = () => {
+      return (e) => {
+        this.next(e);
+      }
+    }
   }
   
   next(e){
     return this._subject.next(e);
   }
+
+  getSubject(){
+    return this._subject;
+  }
+}
+
+
+export class Event {
+  /**
+   * @param {function(Rx.Observer):void} fun 
+   */ 
+  constructor(fun){
+    this._observable = Rx.Observable.create(fun)
+  }
+
+  /**
+   * @param {function(Rx.Observable):Rx.Observable} fun 
+   * @param {any} subRin 
+   */ 
+  getAction(fun, handler){
+
+    return (...events)=>{
+      const observable = fun(this._observable.map((nextEvent)=>{
+        return {...events, nextEvent}
+      }));
+      
+      if(observable && handler){
+        observable.subscribe(handler);
+      }
+    }
+  }
+
+
 }
 
 export default Active;
--- src/Rxjs/Demo.jsx
+++ src/Rxjs/Demo.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import Active from './Active'
+import {Event, Active} from './Active'
 
-export default () => {
-  const active = new Active((subject)=>{
-    subject
-    .throttleTime(1000)
-    .map(event=>{
-      return {x:event.clientX, y: event.clientY}
-    })
-    .subscribe({
-      next: event => console.log(event)
+const event = new Event((observer)=>{
+  observer.next(1);
+  throw new Error(1);
+});
+
+export default() => {
+  const click = event.getAction((observable)=>{
+    return observable.map(e=>{
+      return {event:e.nextEvent}
     });
-  })
+  },{
+    next: x => console.log('got value ',x),
+    error: err => console.error('something wrong occurred: 404',err),
+    complete: () => console.log('done'),
+  });
 
-  const click = (event) => {
-    active.next(event);
-  }
-  
   return (
     <div>
       Please open log to see result. U can't submit a lot time in a second.
