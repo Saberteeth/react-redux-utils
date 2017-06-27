6b8e5933e35773ad58b62a0f8a5f75b912856124
saberteeth <frostmourner@outlook.com>
2017-06-27 17:21:02 +0800

    chore(listview): the scroll view has some debug

--- src/ListView/index.jsx
+++ src/ListView/index.jsx
@@ -249,11 +249,16 @@ export class ListView extends React.Component {
         this.onScroll(top / maxTop, !this.isHideScroll);
       };
     };
+    const mouseDownOutSide = e => {
+      //console.log(e.clientY,this.height);
+      //console.log(e.clientY);
+    
+    }
 
     return (
       <div>
         {this.isHideScroll ? null:(
-          <div className="list-view-scroll" style={{ height: this.height }}>
+          <div onMouseDown={mouseDownOutSide.bind(this)} className="list-view-scroll" style={{ height: this.height }}>
             {this.state.scrollHeight != this.height
               ? <div
                   onMouseDown={mouseDown.bind(this)}
